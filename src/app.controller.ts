997c3d50b35b53e0761e36fd915626462c765ebf
Julian Graefen <juliangraefen@gmail.com>
2023-12-23 20:33:00 +0100

    feat: message endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MessageDto } from './DTO/Message.dto';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,13 @@ export class AppController {
   @Get("/articles")
   getHello(): string {
     return this.appService.getHello();
+
+ 
   }
+  @Post("message")
+  async receiveMessage(@Body()messageDto:MessageDto){
+    this.appService.receiveMessage(messageDto);
+  }
+  
+  
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+
 
 @Injectable()
 export class AppService {
+
+  constructor(
+    private httpService: HttpService
+
+  ) {}
+
   getHello(): string {
     return 'Here you will see the articles that have been published trough Vencio';
+  }
+  async receiveMessage(messageDto) {
+    //const apiUrl = "http://localhost:5000/"
+    const message = messageDto.message.toString();
+    console.log(message);
+  
   }
 }
